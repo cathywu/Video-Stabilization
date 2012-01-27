25929b79422107192ab62babf76993c713889a8f
Cathy Wu <neverisforever@gmail.com>
2012-01-27 14:40:50 -0500

    ring buffer docs

--- RingBuffer.py
+++ RingBuffer.py
@@ -10,12 +10,13 @@ class RingBuffer:
         self.data.extend([default for i in range(self.max-len(self.data))])
 
     def write(self,x):
-        "write value into ring buffer"
+        "writes value into ring buffer"
         self.data[self.index] = x
         if self.index + 1 > self.count:
             self.count = self.index + 1
         self.index = (self.index + 1) % self.max
     def empty(self):
+        "clears data list"
         self.data = [default for i in self.data]
         self.count = 0
 
@@ -23,17 +24,23 @@ class RingBuffer:
         "returns a list of elements from oldest to newest"
         return self.data[self.index:] + self.data[:self.index]
     def get_prev(self,i):
+        "returns nth previous element"
         return self.data[(self.index - i) % self.max]
     def get_last(self):
+        "returns last element"
         return self.get_prev(1)
 
     def set_prev(self,i,value):
+        "sets nth previous element"
         self.data[(self.index - i) % self.max] = value
     def set_last(self,value):
+        "sets last element"
         self.set_prev(1,value)
 
     # FIXME below functions only work for integer values right now, not objects
     def average(self):
+        "returns average of data vector"
         return np.average(self.data)
     def meanmagnitude(self):
+        "returns average magnitude of data vector"
         return np.sqrt(np.dot(self.data,self.data))/self.max
